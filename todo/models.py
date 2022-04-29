cf256204aab9a81cb5b8703e022792a93e45b1f9
kylo74 <kyloc74@gmail.com>
2022-04-29 16:55:50 +0000

    Fix issue with items not displaying

--- todo/models.py
+++ todo/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 
 class Item(models.Model):
-    name = models.CharField(max_length=50, null=False, blank=False)
+    name = models.CharField(max_length=50, null=True, blank=False)
     done = models.BooleanField(null=False, blank=False, default=False)
 
     def __str__(self):
